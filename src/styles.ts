d28be2b81c3d16ca5ec75a8f240d43bd5473cf5d
KVexcavator <str_token@mail.ru>
2020-12-23 19:30:44 +0300

    add styles for container

--- src/styles.ts
+++ src/styles.ts
@@ -1,1 +1,13 @@
 import styled from "styled-components"
+
+export const AppContainer = styled.div `
+  align-items: flex-start;
+  background-color: #3179ba;
+  displey: flex;
+  flex-direction: row;
+  height: 100%;
+  padding: 20px;
+  width: 100%;
+`
+
+
