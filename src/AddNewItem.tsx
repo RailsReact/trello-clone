0e95924d95c76d89601d4ea84861e831a032157e
KVexcavator <str_token@mail.ru>
2020-12-28 21:19:43 +0300

    add task is render

--- src/AddNewItem.tsx
+++ src/AddNewItem.tsx
@@ -1,23 +1,28 @@
 import React , {useState} from "react"
-import {AddItemButton} from "./styles"
+import { NewItemForm } from "./NewItemForm"
 
 interface AddNewItemProps {
-  onAdd(text: string): void,
-  toggleButtonText: string,
+  onAdd(text: string): void
+  toggleButtonText: string
   dark?: boolean
 }
 
 export const AddNewItem = (props: AddNewItemProps) => {
   const [showForm, setShowForm] = useState(false)
-  const { onAdd, toggleButtonText, dark } = props
+  const { onAdd, toggleButtonText } = props
 
   if(showForm){
-    // show item creation form here
+    return (
+      <NewItemForm 
+        onAdd={text => {
+          onAdd(text)
+          setShowForm(false)
+        }}
+      />
+    )
   }
 
   return (
-    <AddItemButton dark={dark} onClick={() => setShowForm(true)}>
-      {toggleButtonText}
-    </AddItemButton>
+    <button onClick={() => setShowForm(true)}>{toggleButtonText}</button>
   )
 }
